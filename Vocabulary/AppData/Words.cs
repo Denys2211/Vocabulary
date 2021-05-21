e3d503bd57db2e88402b66d14291161e01cba387
mac <tsurkanovskyi@gmail.com>
2021-05-21 14:35:32 +0300

    create AppData

---
+++ Vocabulary/AppData/Words.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using SQLite;
+
+namespace Wocabulary.AppData
+{
+    [Table("Words")]
+    public class Words
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+
+        public string Expression { get; set; }
+        public string Result { get; set; }
+        public string DateTime { get; set; }
+    }
+}
---
+++ Vocabulary/Interface/IDataRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using SQLite;
+using Wocabulary.AppData;
+
+namespace Vocabulary.Interface
+{
+    public interface IDataRepository
+    {
+
+        void AddInDataBase(string name, SQLiteConnection connection, double result, params string[] inpu);
+
+        IEnumerable<Words> ReadDataBase(string name, SQLiteConnection connection);
+
+        void DeleteDataTable(string name, SQLiteConnection connection);
+    }
+}
