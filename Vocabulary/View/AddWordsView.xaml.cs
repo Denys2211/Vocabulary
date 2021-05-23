034e251b3cdf373f72b85a5ab67af1c358f07d01
mac <tsurkanovskyi@gmail.com>
2021-05-23 22:19:29 +0300

    Create AddWords page
    * AddWordsView.xaml:
    * VocabularyView.xaml:
    * AddWordsView.xaml.cs:
    * Vocabulary.iOS.csproj:

--- Vocabulary/View/AddWordsView.xaml.cs
+++ Vocabulary/View/AddWordsView.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Vocabulary.ViewModels;
 using Xamarin.Forms;
 
 namespace Vocabulary.View
@@ -10,6 +10,8 @@ namespace Vocabulary.View
         public AddWordsView()
         {
             InitializeComponent();
+
+            BindingContext = new AddWordsViewModel();
         }
     }
 }
---
+++ Vocabulary/ViewModels/AddWordsViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Vocabulary.Model;
+using Xamarin.Forms;
+
+namespace Vocabulary.ViewModels
+{
+    public class AddWordsViewModel : BaseViewModel
+    {
+        private string english;
+        private string ukrainian;
+
+        public AddWordsViewModel()
+        {
+            SaveCommand = new Command(OnSave, ValidateSave);
+            CancelCommand = new Command(OnCancel);
+            this.PropertyChanged +=
+                (_, __) => SaveCommand.ChangeCanExecute();
+        }
+
+        private bool ValidateSave()
+        {
+            return !String.IsNullOrWhiteSpace(english)
+                && !String.IsNullOrWhiteSpace(ukrainian);
+        }
+
+        public string English
+        {
+            get => english;
+            set => SetProperty(ref english, value);
+        }
+
+        public string Ukrainian
+        {
+            get => ukrainian;
+            set => SetProperty(ref ukrainian, value);
+        }
+
+        public Command SaveCommand { get; }
+        public Command CancelCommand { get; }
+
+        private async void OnCancel()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+
+        private async void OnSave()
+        {
+
+            CurrentWordsRepository.AddInDataBase("Words", English, Ukrainian);
+            English = null;
+            Ukrainian = null;
+            OnPropertyChanged("English");
+            OnPropertyChanged("Ukrainian");
+
+            await Shell.Current.GoToAsync("..");
+        }
+    }
+}
